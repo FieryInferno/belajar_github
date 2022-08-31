c652d913bca20aca59d79a95a2031797f73e4b50
FieryInferno <bagassetia271@gmail.com>
2022-08-31 10:23:12 +0700

    feat: Add search location

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Day from './pages/Day';
 import moment from 'moment';
 import axios from 'axios';
 import {stringify} from 'query-string';
+import {Select} from 'antd';
+
+const {Option} = Select;
 
 const App = () => {
   const [weather, setWeather] = useState();
@@ -13,6 +16,7 @@ const App = () => {
   const [weathers, setWeathers] = useState();
   const today = moment().format('YYYY-MM-DD');
   const [todayForecast, setTodayForecast] = useState();
+  const [location, setLocation] = useState();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -87,6 +91,11 @@ const App = () => {
 
   const props = {weather, todayForecast, weathers};
 
+  const onSearch = async (value) => {
+    const response = await axios.get(`http://geodb-free-service.wirefreethought.com/v1/geo/cities?namePrefix=${value}&hateoasMode=false&limit=5`);
+    setLocation(response.data.data);
+  };
+
   /* eslint-disable max-len */
   return (
     <>
@@ -100,12 +109,29 @@ const App = () => {
               <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
               <span className="sr-only">Search</span>
             </button>
-            <div className="hidden relative md:block">
+            <div className="relative w-64">
               <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-                <span className="sr-only">Search icon</span>
+                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
               </div>
-              <input type="text" id="search-navbar" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+              <Select
+                showSearch
+                placeholder="Search location"
+                optionFilterProp="children"
+                onChange={(value) => {
+                  const location = value.split('&');
+                  setLat(location[0]);
+                  setLon(location[1]);
+                }}
+                onSearch={onSearch}
+                // filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+                id="default-search"
+                className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                {location?.map((loc, key) => {
+                  const {latitude, longitude, city, region, country} = loc;
+                  return (<Option value={`${latitude}&${longitude}`} key={key}>{city}, {region}, {country}</Option>);
+                })}
+              </Select>
             </div>
             <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
               <span className="sr-only">Open menu</span>
